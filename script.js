c998d6fd4accea2e5ff5b03ce478fbda918426a7
Aneesh Sharma <ansh993@gmail.com>
2025-01-19 15:31:52 -0500

    info done for first

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for (let i = 0; i < marqueeElementsDisplayed; i++) {
     marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
+
+function sendEmail() {
+    var link = document.getElementById('send_email');
+    var name = document.getElementById('fullname').value;
+    var subject = document.getElementById('subject').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+    var body = `${message}%0D%0A%0D%0AThanks,%0D%0A${name}%0D%0A${phone}`;
+    var email = "info@medleynetworks.com"; 
+    var href = "mailto:" + email + "?subject=" + subject + "&body=" + body;
+    link.setAttribute("href", href);
+}
